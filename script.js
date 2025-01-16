48ab2f5b03681250509eddb3e6c37dbba695082d
Uvie Udjo <derek.ud123@gmail.com>
2025-01-15 21:25:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination',
     clickable: true,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
 // Smooth Scroll for Navigation Links
